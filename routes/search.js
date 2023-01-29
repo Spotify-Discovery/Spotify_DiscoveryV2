b485c61c65c4e7f5513398d0818df0b8b1fce860
milrilowe <milesrhowens@gmail.com>
2023-01-29 06:46:54 -0700

    Added search route

---
+++ routes/search.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+const SPOTIFY_BASE = 'https://api.spotify.com/v1/';
+
+router.get(':token?:q?:type?', (req, res) => {
+  const access_token = req.query.token;
+  const q = req.query.q;
+  const type = req.query.type || 'track,artist';
+  axios.get(`${SPOTIFY_BASE}search?q=${q}&type=${type}&limit=5&market=US`, {
+    headers: {
+      "Authorization": `Bearer ${access_token}`
+    }
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      res.send(response.data);
+    } else {
+      res.status(500).send('Spotify responded with a status ' + response.status);
+    }
+  })
+  .catch((error) => {
+    if (error.response.status === 401) {
+      res.status(401).send();
+    } else {
+      res.status(500).send('Spotify responded with a status ' + error.response.status);
+    }
+  });
+
+});
+
+module.exports = router;
