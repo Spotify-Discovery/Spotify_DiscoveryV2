6e3136e956adfc312486efcc281376fd7117ac9b
milrilowe <milesrhowens@gmail.com>
2023-01-29 06:49:57 -0700

    Added search route

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const auth = require('./auth.js');
 const user = require('./user.js');
 const player = require('./player.js');
 const related = require('./related.js');
+const search = require('./search.js');
 
 const SPOTIFY_BASE = 'https://api.spotify.com/v1/';
 
@@ -15,5 +16,6 @@ router.use('/', auth);
 router.use('/user', user);
 router.use('/player', player);
 router.use('/related', related);
+router.use('/search', search);
 
 module.exports = router;
