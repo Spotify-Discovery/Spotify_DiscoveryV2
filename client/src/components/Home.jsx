6ca3a0a19501311c445a9db214982cd5e0bb7b10
milrilowe <milesrhowens@gmail.com>
2023-02-05 20:06:46 -0700

    Added loading animation while retrieving recommendations

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = ({handleSearch, handleViewChange}) => {
   const [currentList, setCurrentList] = useState('ARTISTS');
   const user = useSelector((state) => state.user);
   const previewSong = useSelector((state) => state.previewSong);
-
+  const feed = useSelector((state) => state.feed);
   const dispatch = useDispatch();
 
   const getHeaderStyle = (listName) => {
@@ -88,7 +88,9 @@ const Home = ({handleSearch, handleViewChange}) => {
       {renderList()}
 
         <div className="feed">
-          {user.feed.map((instance) => {
+          {console.log(feed)}
+          {feed.isLoading && <div class="loading"/>}
+          {feed.feed.map((instance) => {
             return (
               <div>
                 <div className="rec-head-container">
